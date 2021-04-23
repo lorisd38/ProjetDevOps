62aed49daf5a0adb288e7969642e147f07f07eff
MelindaGliana <64643018+MelindaGliana@users.noreply.github.com>
2021-04-23 17:15:05 +0200

    Update TestDataframe.java

--- src/test/java/TestDataframe.java
+++ src/test/java/TestDataframe.java
@@ -5,13 +5,12 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestDataframe {
-
 	@Before
-	public static void setUpBefore() throws Exception {
+	public void setUpBefore() throws Exception {
 	}
 
 	@After
-	public static void tearDownAfter() throws Exception {
+	public void tearDownAfter() throws Exception {
 	}
 
 	@Test
