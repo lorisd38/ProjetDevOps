9931bc8997ce5f351f3dbd6d8024575f761699a7
Laran38 <yulo69@yahoo.fr>
2021-04-24 14:44:30 +0200

    COMMENTS DONE

--- src/main/java/Dataframe.java
+++ src/main/java/Dataframe.java
@@ -2,69 +2,78 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 
+import exception.PandaExceptions;
+import exception.TooManyDataException;
+import exception.TooManyValueException;
+
+/**
+ *
+ * @author ALL Classe principale permettant de contruire le dataframe
+ *
+ */
 public class Dataframe {
 
 	private ArrayList<Series> dataframe;
 
 	protected final String FLOAT = "FLOAT";
 	protected final String INTEGER = "Integer";
-	protected final int MAX_DATA = 100;
-	protected final int MAX_VALUES = 10000;
-	private String splitter = " ";
 	protected final String STRING = "String";
+
+	// Separateur, afin de pouvoir lire tous les types de fichiers CSV
+	private String splitter = " ";
+
+	// Representation de la vigule pour split les floats
 	private String VIRGULE = ",";
 
-	protected Dataframe() {
+	public Dataframe() {
 		this.splitter = ";";
 	}
 
+	/**
+	 * @param PATH le chemin vers un csv
+	 *
+	 */
 	public Dataframe(String PATH) throws Exception {
 		this.splitter = ";";
 		initialisation(toTab(PATH));
 	}
 
+	/**
+	 * @param arguments tableau a deux dimensions de chaine de caracteres
+	 *
+	 */
 	public Dataframe(String[][] arguments) {
-		this(arguments, ";");
-	}
-
-	public Dataframe(String[][] arguments, String splitter) {
-		this.splitter = splitter;
 		initialisation(arguments);
 	}
 
-	protected String ajout(String name, String[] listeObjet) {
-		String valeur = listeObjet[0];
-		if (isInt(valeur))
-			return INTEGER;
-
-		if (isFloat(valeur))
-			return FLOAT;
-
-		return STRING;
-
-	}
-
-	protected Series ajouterTout(Series s, String[][] arguments, int position) {
+	/**
+	 * @param s         Le Series a mettre a jour,
+	 * @param arguments les valeurs
+	 * @param position  l'index de la colonne courante
+	 * @return Series Le Series est mit a jour avec la colonne ajoutee
+	 *
+	 */
+	public Series ajouterTout(Series s, String[][] arguments, int position) {
 		for (int i = 1; i < arguments.length; i++)
 			s.ajouter(arguments[i][position]);
 		return s;
 	}
 
-	protected String getName(String[][] liste, int colonneActuelle) {
+	public String getName(String[][] liste, int colonneActuelle) {
 		return liste[0][colonneActuelle];
 	}
 
 	/**
-	 * @param argument un tableau de donnees
-	 * @return void initialise juste les dataframe
+	 * @param arguments un tableau de donnees
+	 * @return void - initialise juste les dataframe
 	 *
 	 */
-	protected void initialisation(String[][] arguments) {
+	public void initialisation(String[][] arguments) {
 		dataframe = new ArrayList<>();
 		for (int i = 0; i < arguments.length; i++) {
 			String name = getName(arguments, i);
 			Series s = null;
-			switch (ajout(name, arguments[i])) {
+			switch (type(arguments[i])) {
 			case INTEGER:
 				s = new Series<Integer>();
 				break;
@@ -85,37 +94,79 @@ public class Dataframe {
 		}
 	}
 
-	protected boolean isFloat(String valeur) {
+	/**
+	 *
+	 * @param valeur
+	 * @return 1 si valeur est un float, 0 sinon
+	 */
+	public boolean isFloat(String valeur) {
 		String[] all = valeur.split(VIRGULE);
 
 		return all.length == 2 && isInt(all[0]) && isInt(all[1]);
 	}
 
-	protected boolean isInt(String valeur) {
+	/**
+	 *
+	 * @param valeur
+	 * @return 1 si valeur est un entier, 0 sinon
+	 */
+	public boolean isInt(String valeur) {
 		return valeur.matches("-?\\d+");
 	}
 
-	private String[] nextLine(String line) throws TooManyDataException {
+	/**
+	 *
+	 * @param line
+	 * @return un tableau de chaine de caractere correspondant a la ligne decoupee
+	 * @throws TooManyDataException si trop de donnees sont a gerer
+	 */
+	public String[] nextLine(String line) throws TooManyDataException {
 		String[] toRet = line.split(splitter);
-		if (toRet.length >= MAX_DATA)
+		if (toRet.length >= PandaExceptions.MAX_DATA)
 			throw new TooManyDataException();
 		return toRet;
 	}
 
-	protected String[][] toTab(String PATH) throws Exception {
-		String[][] tab = new String[MAX_VALUES][MAX_DATA];
+	/**
+	 *
+	 * @param PATH le chemin vers un fichier CSV
+	 * @return le tableau de chaine de caractere correspondant aux valeurs dans le
+	 *         fichier
+	 * @throws TooManyValuesException si les colonnes sont trop grandes
+	 * @throws Exception              si le fichier n'existe pas, geree par le
+	 *                                lecteur de fichier
+	 */
+	public String[][] toTab(String PATH) throws Exception {
+		String[][] tab = new String[PandaExceptions.MAX_VALUES][PandaExceptions.MAX_DATA];
 		BufferedReader sc = new BufferedReader(new FileReader(PATH));
 		int nbLine = 0;
 		String line = sc.readLine();
 		while (line != null) {
 			tab[nbLine++] = nextLine(line);
-			if (nbLine == MAX_VALUES)
+			if (nbLine == PandaExceptions.MAX_VALUES)
 				throw new TooManyValueException();
 			line = sc.readLine();
 		}
 		sc.close();
 
 		return tab;
+	}
+
+	/**
+	 *
+	 * @param listeObjet, une colonne du tableau
+	 * @return une chaine de caracteres correspondant au types de donnees
+	 */
+	public String type(String[] listeObjet) {
+		String valeur = listeObjet[0];
+		if (isInt(valeur))
+			return INTEGER;
+
+		if (isFloat(valeur))
+			return FLOAT;
+
+		return STRING;
+
 	}
 
 }
