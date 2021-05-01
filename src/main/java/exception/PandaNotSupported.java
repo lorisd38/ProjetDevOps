aa1fd05dfb9781b2a6821a1940923a31369a0afa
pixel38320 <pixel38320@gmail.com>
2021-05-01 18:18:40 +0200

    Try to skip method for jacoco

--- src/main/java/exception/PandaNotSupported.java
+++ src/main/java/exception/PandaNotSupported.java
@@ -3,6 +3,7 @@ package exception;
 @SuppressWarnings("serial")
 public class PandaNotSupported extends PandaExceptions {
 	@Override
+	@ExcludeFromJacocoGeneratedReport
 	public void printStackTrace() {
 		super.printStackTrace();
 		System.err.println("Les types possibles pour les colonnes du dataframe sont String, Float ou Int.");
